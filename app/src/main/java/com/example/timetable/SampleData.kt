f23e6c2c6cc8dd3499a78eb1523b50b0dfcaf2c2
aziz <19atabaev.aziz@gmail.com>
2021-10-26 23:06:32 +0500

    Project is added

---
+++ app/src/main/java/com/example/timetable/SampleData.kt
@@ -0,0 +1,51 @@
+package com.example.timetable
+
+import android.os.Message
+
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "",
+            "Monday\n" +
+                    "1. Dasturiy ta'minot tizimlarini loyihalash\n" + "Type - Tajriba\n" + "Teacher - Bazarboyev Murotjon X\n" + "Cabinet - 319\n" +
+                    "2. Pedagogika. Psixologiya\n" + "Type - Seminar\n" + "Teacher - Abdullayeva Sh.\n" + "Cabinet - 214\n" +
+                    "3. Inson-kompyuter o'zaro ta'siri\n" + "Type - Amaliyot\n" + "Teacher - Matyaqubov M.\n" + "Cabinet - 313"
+        ),
+        Message(
+            "",
+            "Tuesday\n" +
+                    "1. Inson-kompyuter o'zaro ta'siri\n" + "Type - Ma'ruza\n" + "Teacher - Matyaqubov M.\n" + "Cabinet - 321\n" +
+                    "2. Dasturlash uslublari va paradigmalar\n" + "Type - Amaliyot\n" + "Teacher - Axmedov E.\n" + "Cabinet - 321\n" +
+                    "3. None"
+        ),
+        Message(
+            "",
+            "Wednesday\n" +
+                    "1. Komputer tarmoqlari\n" + "Type - Ma'ruza\n" + "Teacher - Yangibayeva M.\n" + "Cabinet - 212\n" +
+                    "2. Dasturlash uslublari va paradigmalar\n" + "Type - Amaliyot\n" + "Teacher - Axmedov E.\n" + "Cabinet - 321\n" +
+                    "3. Inson-kompyuter o'zaro ta'siri\n" + "Type - Ma'ruza\n" + "Teacher - Aliyev O.\n" + "Cabinet - 315"
+        ),
+        Message(
+            "",
+            "Thursday\n" +
+                    "1. Dasturlash uslublari va paradigmalar\n" + "Type - Ma'ruza\n" + "Teacher - Axmedov E.\n" + "Cabinet - 114\n" +
+                    "2. Pedagogika. Psixologiya\n" + "Type - Ma'ruza\n" + "Teacher - Abdullayeva Sh.\n" + "Cabinet - 212\n" +
+                    "3. None"
+        ),
+        Message(
+            "",
+            "Friday                                              \n" +
+                    "1. Komputer tarmoqlari\n" + "Type - Ma'ruza\n" + "Teacher - Yangibayeva M.\n" + "Cabinet - 212\n" +
+                    "2. Murabbiylik soati\n" +
+                    "3. None"
+        ),
+        Message(
+            "",
+            "Saturday\n" +
+                    "1. Dasturiy ta'minot tizimlarini loyihalash\n" + "Type - Amaliyot\n" + "Teacher - Turdiyev T.\n" + "Cabinet - 321\n" +
+                    "2. Komputer tarmoqlari\n" + "Type - Tajriba\n" + "Teacher - Abdullayeva Sh.\n" + "Cabinet - 216\n" +
+                    "3. Inson-kompyuter o'zaro ta'siri\n" + "Type - Ma'ruza\n" + "Teacher - Matyaqubov M.\n" + "Cabinet - 325"
+        )
+    )
+}
